8a8a9257c5ded4bbd1b43772421bd5e1894dfe26
Parth Shimpi <pas201@cam.ac.uk>
2020-10-07 22:50:07 +0100

    Added highlighting

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,19 @@
 // reads site data and sends to popup
 
-alert( $.ajax("?event=1117").responseText )
+$.get("?event=1117", (data) => { alert(data) });
+
+// Highlight group buttons
+
+var links = document.getElementsByTagName("a");
+
+chrome.storage.sync.get('group', function (data) {
+  var c = data.group;
+  for (let i = 0; i < links.length; i++) {
+    const element = links[i];
+    let re = /GROUP (A|B|C|D)$/;
+    var val = re.exec(element.innerHTML)
+    if (val) {
+      if (val[1] == c) { element.style.backgroundColor = "#a6eded"; } else { element.style.backgroundColor = "#e6eded"; } 
+    }
+  }
+});
