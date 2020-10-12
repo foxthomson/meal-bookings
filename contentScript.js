6b8f7bfc3cb9ed2179efc23953de3d6cbd3085e8
foxthomson <foxthomson0@gmail.com>
2020-10-12 21:58:06 +0100

    Get next weeks food as well

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,19 @@
-var groupcheck = /GROUP (A|B|C|D)$/
+var groupcheck = /GROUP (A|B|C|D)$/;
 var timecheck = /(?<=Cafeteria\s)[0-9.-\s]{0,15}(?=\s-)/
 var idcheck = /(?<=href=")[a-z0-9?=]{0,50}(?=")/
 var daycheck = /^(Mon|Tue|Wed|Thurs|Fri|Sat|Sun)/;
+var nextpagecheck = /Later/;
 
 function geteventscore(links, answer, ansfun) {
     // Runs process dates on each link
     if (links.length == 0) {
         ansfun(answer);
     } else {
-        $.get(links[0], processdates(links.slice(1), links[0], answer, ansfun));
+        $.get(links[0].link, processdates(links.slice(1), links[0], answer, ansfun));
     }
 }
 
-function getevents(groupname, ansfun) {
+function getevents(groupname, ansfun=false) {
     // finds all the link's to events (and colours the links) then starts geteventscore
     var links = [];
     var as = document.getElementsByTagName("a");
@@ -22,16 +23,18 @@ function getevents(groupname, ansfun) {
         if (groupdata) {
             if (groupdata[1] == groupname.group) {
                 var eventlink = current.href;
-                links.push(eventlink);
+                links.push({link: eventlink, firstpage: true});
                 current.style.backgroundColor = "#a6eded"; 
             }   
             else { current.style.backgroundColor = "#e6eded"; }
         }
     }
-    geteventscore(links, [], ansfun);
+    if (ansfun) {
+        geteventscore(links, [], ansfun);
+    }
 }
 
-function processdates(links, link, answer, ansfun) {
+function processdates(links, linkdat, answer, ansfun) {
     // Wraper for process dates so it can be passed data from outside the $.get
     return function(html) {
         // gets the events then passes control back to geteventscore
@@ -40,10 +43,15 @@ function processdates(links, link, answer, ansfun) {
         var titlestring = parsed.getElementsByTagName("h1")[0].innerText;
         var time = timecheck.exec(titlestring)[0];
         var possibledates = parsed.getElementsByTagName("a");
+        var firstpage = linkdat.firstpage
         for (let i = 0; i < possibledates.length; i++) {
             const current = possibledates[i];
             if (daycheck.test(current.innerHTML)) {
-                answer.push({link: link, data: current.innerText, time: time});
+                answer.push({link: linkdat.link, date: current.innerText, time: time});
+            }
+            if (firstpage && nextpagecheck.test(current.innerHTML)) {
+                links.push({link: current.href, firstpage: false});
+                firstpage = false;
             }
         }
         geteventscore(links, answer, ansfun);
