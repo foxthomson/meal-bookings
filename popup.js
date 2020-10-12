8db3b9462039c1ada06646a4f5580abecd412b07
Parth Shimpi <pas201@cam.ac.uk>
2020-10-12 22:25:12 +0100

    Made popup table

--- popup.js
+++ popup.js
@@ -4,10 +4,35 @@ chrome.storage.sync.get('group', gettable);
 
 function gettable(group) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { group: group }, maketable);
+        chrome.tabs.sendMessage(tabs[0].id, { group: group.group }, maketable);
     });
 }
-
-function maketable(table) {
+function maketable(data) {
     // make the table
+    let table = document.querySelector("table");
+    for (let element of data) {
+        let row = table.insertRow();
+
+        let linknode = document.createElement("a")
+        linknode.href = element.link 
+        linknode.innerText = "<td>"+ element.date + "</td>" + " <td>" + processtime(element.time) + "</td>";
+    }
+}
+
+function processtime(time) {
+    if (time == '1230-1300') {
+        return 'Early Lunch';
+    }
+    else if (time == '1320-1400') {
+        return 'Late Lunch';
+    }
+    else if (time == '1800-1845') {
+        return 'Early Dinner';
+    }
+    else if (time == '1915-2000') {
+        return 'Late Dinner';
+    }
+    else {
+        return 'Bruhhhhhhh'
+    }
 }
